0d2ff5cd4cf9e1c04aa8eda42515ddbe781226ad
cihan <cihan@cihan.us>
2014-01-06 02:47:11 +0200

    app.py added
    all planned steps written as comments

---
+++ app.py
@@ -0,0 +1,15 @@
+# assume that our r is 1
+#generate a random float for x in 0,1
+
+#generate a random float for y in 0,1
+
+#calculate the hypotenuse
+
+#if hypotenuse is equal or less than 1 it means dart hitting shaded area 
+
+
+#if not do nothing.
+
+#get sum of tries (all)
+
+#calculate pi with formula ======> pi = 4* (shaded hits)/tries
